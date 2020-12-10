1c2af138049c7f7c1a6b03d9246f9228da418bdb
MISTIKMAKS <mistikmaks80885@gmail.com>
2020-12-10 10:35:57 +0200

    first master commit

---
+++ pr5_5/pr5_5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double f(double x)
+{
+	return pow(x, 3) - 2 * pow(x, 2) - 6 * x - 1;
+}
+typedef double (*function_f)(double x);
+
+double root(function_f f_, double a, double b, double eps)
+{
+	double c;
+	if (fabs(b - a) < eps)
+		return (a + b) / 2;
+	else
+	{
+		c = (a + b) / 2;
+		if (f_(a) * f_(c) <= 0.)
+			return root(f_, a, c, eps);
+		else
+			return root(f_, c, b, eps);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	double a, b, epc;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "epc = "; cin >> epc;
+	std::cout << root(&f, a, b, epc) << std::endl;
+	return 0;
+}
